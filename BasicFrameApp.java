084b3012f1d8fd19eb9d072a3cc368aa2099599c
MmaThapelo2002 <nkonemmathapelo@gmail.com>
2025-07-28 11:55:17 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author ERC
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyFirstFrame myFrame;
+        
+        myFrame = new MyFirstFrame();
+    }
+    
+}
